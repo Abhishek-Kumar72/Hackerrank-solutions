a5a3d900d8cdad08ea4b50e1f5fbaad1276a0015
Abhishek Kumar <142827416+Abhishek-Kumar72@users.noreply.github.com>
2026-09-25 18:28:41 +0530

    ✅ Employee Names [mysql] — approach1

---
+++ solutions/mysql/easy/name-of-employees/solution-approach1.sql
@@ -0,0 +1,13 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/name-of-employees/problem?isFullScreen=true
+-- Problem     Employee Names
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-09-25, 06:28 p.m.
+-- ──────────────────────────────────────────────────
+
+select name from employee 
+order by name ASC;
